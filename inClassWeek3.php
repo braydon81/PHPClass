8e80946ad44418cfa9441a82d351ef8e7d99fa1a
Braydon Johnson <braydon85_2@hotmail.com>
2015-04-02 17:20:11 -0500

    In class Week 3
    in class code

---
+++ inClassWeek3.php
@@ -0,0 +1,46 @@
+<?php
+    $testArray = array(1,2,3);
+    $stringArray = array("one", "two", "three");
+    $x = 1;
+
+    //echo $testArray[0];
+    //echo "<br/>";
+    //echo $stringArray[2];
+    
+
+    /*do{
+        echo nl2br("\n X is: " . $x);
+        $x++;
+    }
+    while($x < 10);
+
+
+    for($i = 0; $i <= 10; $i++)
+    {
+        echo nl2br("\nThis is the value of I: " . $i);   
+    }
+    $colorArray= array("yellow", "blue", "green", "orange");
+
+    foreach($colorArray as $y)
+    {
+        echo nl2br("\n Color is: " . $y);
+    }*/
+
+    $var = "dog";
+    switch($var)
+    {
+        case "cat":
+        echo "This is a cat";
+        break;
+        
+        case "horse":
+        echo "This is a horse";
+        break;
+        
+        case "dog":
+        echo "This is a dog";
+        break;
+        
+    }
+
+?>
